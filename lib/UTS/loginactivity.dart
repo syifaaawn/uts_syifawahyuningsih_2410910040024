a3590dd004c42a865ff088b5e0f52fa726444278
syifaaawn <syifawahyuningsih25@gmail.com>
2025-10-23 19:09:25 +0700

    update

---
+++ lib/UTS/loginactivity.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Loginactivity extends StatelessWidget {
+  const Loginactivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
